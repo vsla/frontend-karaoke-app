787f091270e346f7d39cc972afa2fb36b5f8de46
victor attar <vsla@cin.ufpe.br>
2024-05-31 16:25:45 -0300

    Updating order of the videos

--- src/components/VideoQueue.jsx
+++ src/components/VideoQueue.jsx
@@ -1,62 +1,69 @@
 import React from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa6";
 
 const VideoQueue = ({ videos, removeVideo, updateVideoOrder }) => {
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
+  const moveVideoUp = (index) => {
+    if (index === 0) return;
+    const newQueue = [...videos];
+    [newQueue[index - 1], newQueue[index]] = [
+      newQueue[index],
+      newQueue[index - 1],
+    ];
+    updateVideoOrder(newQueue);
+  };
 
-    const reorderedVideos = Array.from(videos);
-    const [removed] = reorderedVideos.splice(result.source.index, 1);
-    reorderedVideos.splice(result.destination.index, 0, removed);
-
-    updateVideoOrder(reorderedVideos);
+  const moveVideoDown = (index) => {
+    if (index === videos.length - 1) return;
+    const newQueue = [...videos];
+    [newQueue[index], newQueue[index + 1]] = [
+      newQueue[index + 1],
+      newQueue[index],
+    ];
+    updateVideoOrder(newQueue);
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable-video">
-        {(provided) => (
-          <div
-            {...provided.droppableProps}
-            className="divide-y divide-gray-600 max-w-2xl"
-            ref={provided.innerRef}
-          >
-            {videos.map((video, index) => (
-              <Draggable key={video.id} draggableId={video.id} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className=" px-2 py-1 flex items-center justify-center pt-2 my-1"
-                  >
-                    <img
-                      src={video.thumbnail}
-                      alt={video.title}
-                      style={{
-                        width: "50px",
-                        height: "auto",
-                        marginRight: "10px",
-                      }}
-                    />
-                    <div className="flex grow text-sm">
-                      {video.title} ({video.user})
-                    </div>
-                    <button
-                      className="py-1 px-2 items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                      onClick={() => removeVideo(video.videoId)}
-                    >
-                      Remover
-                    </button>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
+    <>
+      {videos.map((video, index) => {
+        return (
+          <div className=" px-2 py-1 flex items-center justify-center pt-2 my-1">
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              style={{
+                width: "50px",
+                height: "auto",
+                marginRight: "10px",
+              }}
+            />
+            <div className="flex grow text-sm flex-col">
+              <p>{video.title}</p>
+              <p className="text-gray-400">{video.user}</p>
+            </div>
+            <button
+              className="py-1 px-2 items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+              onClick={() => removeVideo(video.videoId)}
+            >
+              Remover
+            </button>
+            <div className="ml-2 flex flex-row border border-gray-400 rounded divide-x">
+              <button
+                className="px-0.5 py-1"
+                onClick={() => moveVideoUp(index)}
+              >
+                <FaArrowUp className="text-sm" />
+              </button>
+              <button className="p-0.5">
+                <FaArrowDown
+                  className="text-sm"
+                  onClick={() => moveVideoDown(index)}
+                />
+              </button>
+            </div>
           </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+        );
+      })}
+    </>
   );
 };
 
