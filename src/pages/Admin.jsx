ef5a725c69e7ab28b0354dea6843c5dfd495c75d
victor attar <victor.attar@shipwell.com>
2024-11-15 17:42:38 -0300

    Add delete party

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,5 @@
-import api from "api/api";
 import React, { useEffect, useState } from "react";
-
+import api from "api/api";
 
 const Admin = () => {
   const [parties, setParties] = useState([]);
@@ -13,23 +12,53 @@ const Admin = () => {
     fetchParties();
   }, []);
 
+  const handleDeleteParty = async (partyCode) => {
+    try {
+      await api.delete(`/api/party/${partyCode}`);
+      setParties(parties.filter((party) => party.code !== partyCode));
+    } catch (error) {
+      console.error("Erro ao remover festa", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Painel de Administração</h1>
-      <table>
-        <thead>
+    <div className="p-6">
+      <h1 className="mb-4">Painel de Administração</h1>
+      <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th>Código da Festa</th>
-            <th>Número de Vídeos</th>
+            <th className="py-3 px-6">Código da Festa</th>
+            <th className="py-3 px-6">Número de Vídeos</th>
+            <th className="py-3 px-6">Ações</th>
           </tr>
         </thead>
         <tbody>
-          {parties.map((party) => (
-            <tr key={party.code}>
-              <td>{party.code}</td>
-              <td>{party.videos.length}</td>
+          {parties.length === 0 ? (
+            <tr>
+              <td colSpan="3" className="py-4 px-6 text-center">
+                Nenhuma festa disponível
+              </td>
             </tr>
-          ))}
+          ) : (
+            parties.map((party) => (
+              <tr
+                key={party.code}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 "
+              >
+                <td className="py-4 px-6">{party.code}</td>
+                <td className="py-4 px-6 ">{party.videos.length}</td>
+                <td className="py-4 px-6 ">
+                  <button
+                    type="button"
+                    className="btn btn-red btn-sm"
+                    onClick={() => handleDeleteParty(party.code)}
+                  >
+                    Deletar
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
